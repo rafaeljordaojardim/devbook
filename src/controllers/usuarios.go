3f390a63d73e9bec260f34ce4a28c09e5d25d042
Rafael Jardim <jordao.jardim@hotmail.com.br>
2021-02-05 06:32:46 -0300

    add user controller

---
+++ src/controllers/usuarios.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+// CriarUsuario usuario no banco
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// GetUsuario pega usuario no banco
+func GetUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// GetUsuarios usuarios no banco
+func GetUsuarios(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// AtualizarUsuario atualiza usuario no banco
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeletaUsuario usuario no banco
+func DeletaUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
