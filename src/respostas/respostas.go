24b5fce69da7f53e92cf33d018fb465abbdb52b0
Rafael Jardim <jordao.jardim@hotmail.com.br>
2021-02-06 05:42:33 -0300

    feat: add responses formatted

---
+++ src/respostas/respostas.go
@@ -0,0 +1,24 @@
+package respostas
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// JSON retorna uma resposta formatada em JSON
+func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
+	w.WriteHeader(statusCode)
+	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+		log.Fatal(erro)
+	}
+}
+
+// Erro retorna um erro em formato json
+func Erro(w http.ResponseWriter, statusCode int, erro error) {
+	JSON(w, statusCode, struct {
+		Erro string `json:"json"`
+	}{
+		Erro: erro.Error(),
+	})
+}
