940ea37f9b11b09e200f40ac87a79162494eb9bc
Rafael Jardim <jordao.jardim@hotmail.com.br>
2021-02-06 05:30:25 -0300

    feat: receive the request and send to repository

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,12 +1,42 @@
 package controllers
 
 import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 // CriarUsuario usuario no banco
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
 
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro := json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id inserido %d", usuarioID)))
 }
 
 // GetUsuario pega usuario no banco
