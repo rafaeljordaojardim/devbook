e540dcf953ee2314b5b7d1157d4027ccf84bf156
Rafael Jardim <jordao.jardim@hotmail.com.br>
2021-02-07 20:30:35 -0300

    feat: padronizando respostas

--- src/respostas/respostas.go
+++ src/respostas/respostas.go
@@ -8,9 +8,12 @@ import (
 
 // JSON retorna uma resposta formatada em JSON
 func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
-		log.Fatal(erro)
+	if dados != nil {
+		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+			log.Fatal(erro)
+		}
 	}
 }
 
