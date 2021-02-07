6970a1e033780a3028fdabacc1baf1c99834c84f
Rafael Jardim <jordao.jardim@hotmail.com.br>
2021-02-07 20:29:23 -0300

    feat: validar e preparar usuario

--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -1,13 +1,52 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario representa usuario utilizando a rede social
 type Usuario struct {
-	ID       uint      `json:"id,omitempty"`
+	ID       uint64    `json:"id,omitempty"`
 	Nome     string    `json:"nome,omitempty"`
 	Nick     string    `json:"nick,omitempty"`
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+// Preparar recebe metodos para validar e formatar usuario
+func (usuario *Usuario) Preparar(etapa string) error {
+	if erro := usuario.validar(etapa); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+func (usuario *Usuario) validar(etapa string) error {
+	if usuario.Nome == "" {
+		return errors.New("O Nome é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O Nick é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O Email é obrigatório e não pode estar em branco")
+	}
+
+	if etapa == "cadastro" && usuario.Senha == "" {
+		return errors.New("O Senha é obrigatório e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
