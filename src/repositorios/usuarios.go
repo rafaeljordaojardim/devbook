a5cfcc472e2e2c165f13f7b11290f65e739640e6
Rafael Jardim <jordao.jardim@hotmail.com.br>
2021-02-08 21:53:12 -0300

    deletar do banco

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -109,3 +109,17 @@ func (repositorio Usuarios) Atualizar(ID uint64, usuario modelos.Usuario) error 
 
 	return nil
 }
+
+// Deletar exclui as informações de um usuario no banco
+func (repositorio Usuarios) Deletar(ID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from usuarios where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(ID); erro != nil {
+		return erro
+	}
+	return nil
+}
