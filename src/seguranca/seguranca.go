7a1eb33f5f3ee6f0c2d2fc272a8df6df3ee9e2e8
Rafael Jardim <jordao.jardim@hotmail.com.br>
2021-02-09 06:25:44 -0300

    feat: adicionando seguranca

---
+++ src/seguranca/seguranca.go
@@ -0,0 +1,13 @@
+package seguranca
+
+import "golang.org/x/crypto/bcrypt"
+
+// Hash recebe uma string e coloca um Hash nela
+func Hash(senha string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(senha), bcrypt.DefaultCost)
+}
+
+// VerificarSenha compara a senha com o hash do banco
+func VerificarSenha(senhaComHash, senhaString string) error {
+	return bcrypt.CompareHashAndPassword([]byte(senhaComHash), []byte(senhaString))
+}
